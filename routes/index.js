cdce17824b930a024910bc45f61882e597dbf656
jorge <jorgecardozosilvio@gmail.com>
2018-04-28 12:58:44 -0300

    Se agrego la parte de la base de datos

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const mongoose = require('mongoose');
-const db_uri = "mongodb://localhost:27017/node_proy";
-const modeloUsuarios = require('../models/usuarios')
+//const db_uri = "mongodb://localhost:27017";
+const modeloUsuarios = require('../models/usuarios').Usuarios;
 
-mongoose.connect(db_uri);
+
+//mongoose.connect(db_uri);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -15,6 +16,40 @@ router.get('/', function(req, res, next) {
 
 router.get('/register',function(req,res,next){
   res.render('register');
+});
+
+/*si se realiza un post en Register*/
+router.post('/register',function(req,res,next){
+  
+  /*Acá formo un modelo del usuario para insertar en la base de datos*/
+  var u1=new modeloUsuarios({
+      
+      nombre:req.body.nombre,
+      apellido:req.body.apellido,
+      email:req.body.email,
+      password:req.body.password
+  })
+
+  /*Esta consulta es para comprobar si hay un usuario con la misma dirección de Email*/
+ modeloUsuarios.find({})
+             .where('email').equals(req.body.email)
+              .exec(obtenerUsuarios);
+              function obtenerUsuarios(err,Us){
+
+                    if(err)
+                      console.log("Error: "+err);
+                    else{
+                          if(Us==""){
+                           u1.save(); /*esto guarda los datos de "u1" en la base de datos "DATOS", en la tabla "usuarios"*/
+                           res.send("Reibimos los datos ");
+                         }
+                          else{ /*Acá puede ir el codigo para mostrar en el REGISTER, que el mail ingresado ya fue utilizado
+                                por ahora muestro un cartel*/
+                                console.log("El mail ya fue utilizado");
+                                res.render('register');
+                          }  
+                    }
+                };
 });
 
 // Redirect the user to Twitter for authentication.  When complete,
