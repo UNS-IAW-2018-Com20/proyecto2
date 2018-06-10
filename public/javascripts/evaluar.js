d278b03b5172a50ed7a2cef2310cba2106bf1711
Juan <juan.manuel.iglesias93@gmail.com>
2018-06-10 17:40:58 -0300

    Promedio criterios

--- public/javascripts/evaluar.js
+++ public/javascripts/evaluar.js
@@ -11,15 +11,18 @@ $("#formEvaluacion").submit(function(event){
 $(document).ready(function() {
 
   $(".criterioSelect").change(function(){
+    console.log("dale");
     let i;
     let total = 0;
     //cantidad de criterios
     let cantidadCriterios = $("#cantidadCriterios").val();
+    console.log(cantidadCriterios);
 
     for (i=0;i<cantidadCriterios;i++){
-      total+= parseInt($("#criterio"+i).find(":selected").text());
+      console.log($("#criterio"+i).find(":selected").val());
+      total+= parseInt($("#criterio"+i).find(":selected").val());
     }
-
+    console.log(total);
     $("#nota_general").text(total/cantidadCriterios);
   });
 });
