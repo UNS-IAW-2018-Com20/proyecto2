2359c49b0496baf25397a7e99026bdddbd06de3d
Juan <juan.manuel.iglesias93@gmail.com>
2018-06-12 19:54:02 -0300

    Mejora en las vistas

--- app.js
+++ app.js
@@ -49,14 +49,10 @@ passport.use(new LocalStrategy(
         if (user === null){
           return done(null,false, {message: 'El usuario o la contraseña son incorrectos.'});
         }
-        console.log("why");
-        console.log(user.password);
-        console.log(password);
+        
         var salt = bcrypt.genSaltSync(10);
         var hash = bcrypt.hashSync(password,salt);
-        console.log(hash);
         bcrypt.compare(password, user.password, function(err, res) {
-          console.log(res);
           if (res===false){
             return done(null, false, {message: 'El usuario o la contraseña son incorrectos.'});
           }
